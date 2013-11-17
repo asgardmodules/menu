897a2741c7b53cbb75bc2b6c0bff3554d41cc96b
unknown <Michel>
2013-11-17 18:55:41 +1100

    fixes

--- bundle.php
+++ bundle.php
@@ -2,13 +2,9 @@
 namespace App\Menu;
 
 class Bundle extends \Coxis\Core\BundleLoader {
-	public function load($queue) {
-		parent::load();
-	}
-
 	public function run() {
-		\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Menus', 'link' => 'menus');
-		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('menu/icon.svg'), 'link'=>'menus', 'title' => 'Menus', 'description' => __('Manage the menus.'));
+		\Coxis\Admin\Libs\AdminMenu::instance()->menu[0]['childs'][] = array('label' => 'Menus', 'link' => 'menus');
+		\Coxis\Admin\Libs\AdminMenu::instance()->home[] = array('img'=>\URL::to('menu/icon.svg'), 'link'=>'menus', 'title' => 'Menus', 'description' => __('Manage the menus.'));
 		parent::run();
 	}
 }
